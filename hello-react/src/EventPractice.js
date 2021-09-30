636796d22119084593c08cab023ff4c8033e9d0d
changyu <changyu.park1@gmail.com>
2021-09-30 18:14:25 +0900

    p 131

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -4,6 +4,24 @@ class EventPractice extends Component{
     state = {
         message : ''
     }
+
+    constructor(props){
+        super (props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({
+            message: e.target.value
+        });
+    }
+    handleClick (){
+        alert(this.state.message);
+        this.setState({
+            message:''
+        });
+    }
     render(){
         return (
             <div>
@@ -13,22 +31,9 @@ class EventPractice extends Component{
                 name = "message"
                 placeholder = "아무거나 입력해 보세요"
                 value = {this.state.message}
-                onChange={
-                    (e)=>{
-                        this.setState({
-                            message : e.target.value
-                        })
-                    }
-                }
+                onChange={this.handleChange}
                 />
-                <button onClick = {
-                    () => {
-                        alert(this.state.message);
-                        this.setState({
-                            message:'' // 여기서 다시 초기화 
-                        });
-                    }
-                }>확인</button>
+                <button onClick = {this.handleClick}>확인</button>
             </div>
         );
     }
