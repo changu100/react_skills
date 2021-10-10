46c60d9e35ae55134ff182fa1511001a7adb8259
changyu <changyu.park1@gmail.com>
2021-10-11 00:28:38 +0900

    p162

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,17 +1,11 @@
 import React ,{Component} from 'react';
-import ScrollBox from './ScrollBox'
+import IterationSample from './IterationSample'
 
 class App extends Component{
   render(){
     return(
-      <div>
-        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
-        <button onClick={()=> this.scrollBox.scrollToBotton()}>
-          맨 밑으로
-        </button>
-      </div>
+      <IterationSample/>
     )
-
   }
   
 }
