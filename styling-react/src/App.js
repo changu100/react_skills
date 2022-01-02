b9b974692c6c3a44e89fcd45cccc7946778b52ad
changyuPark <blue_rose17@naver.com>
2022-01-02 15:39:52 +0900

    9장

---
+++ styling-react/src/App.js
@@ -0,0 +1,15 @@
+
+import SassComponent from './SassComponent';
+import React, { Component } from 'react';
+
+class App extends Component() {
+  render(){
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
