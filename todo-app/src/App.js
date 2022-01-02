c4d0110ec8006ec72efda646032f4b7b6c4e4782
changyuPark <blue_rose17@naver.com>
2022-01-02 20:19:35 +0900

    todo app 작업중

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-import TodoTemplate from './component/TodoTemplate';
+import './App.scss';
+import TodoInsert from "./components/TodoInsert";
+import TodoTemplate from './components/TodoTemplate';
+import TodoList from './components/TodoList';
+
 
 const App = ()=>{
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>
+  return <TodoTemplate>
+    <TodoInsert/>
+    <TodoList/>
+  </TodoTemplate>
 };
 export default App;
