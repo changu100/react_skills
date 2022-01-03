4d4674baab28bc8047f63978cb97471726450461
changu100 <blue_rose17@naver.com>
2022-01-03 22:15:30 +0900

    10장 종료

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { MdAdd } from "react-icons/md";
 import  './TodoInsert.scss'
 //https://react-icons.netlify.com/#/icons/md
 
 
 
-const TodoInsert = ()=>{
+const TodoInsert = ({onInsert})=>{
+    const [value, setValue] = useState('');
+    const onChange = useCallback(e=>{
+        setValue(e.target.value);
+    },[])
+
+    const onSubmit = useCallback(e=>{
+        onInsert(value);
+        setValue('')// value 갑 초기화
+
+        e.preventDefault();
+    },
+    [onInsert,value],
+    )
+
+    // const onClick= useCallback(()=>{
+    //     onInsert(value);
+    //     setValue('');
+    // },[onInsert,value]);
     return (
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요"/>
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+                placeholder="할 일을 입력하세요"
+                value={value}
+                onChange={onChange}
+            />
             <button type="submit">
+            {/* <button onClick={onClick}> */}
                 <MdAdd/>
             </button>
         </form>
